7e90b598068d397b846f68c4dfe29380a8001832
Artiver <l952011340@qq.com>
2024-07-25 21:46:28 +0800

    fix: 支持代码块样式

--- src/views/main/editor/markdown/MarkdownEditor.vue
+++ src/views/main/editor/markdown/MarkdownEditor.vue
@@ -8,17 +8,22 @@ const vditor = ref()
 
 onMounted(() => {
   vditor.value = new Vditor('markdown-editor',{
-    cdn: '/static/markdown'
+    cdn: '/static/markdown',
+    cache: {enable: false},
   })
 })
 </script>
 
 <template>
   <div class="app-container">
+    <div id="markdown-title">Title</div>
     <div id="markdown-editor"></div>
   </div>
 </template>
 
 <style scoped lang="scss">
-
+#markdown-title {
+  height: 50px;
+  text-align: center;
+}
 </style>
